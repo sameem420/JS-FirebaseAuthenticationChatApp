afb225394a3e2b35495e2e7d4a2fc19b6724175d
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-09-25 18:46:28 +0500

    added the login UI

---
+++ app.js
@@ -0,0 +1,26 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCkIxHNAMhTSYNGr0n97Nd8bb5ciazZvCc",
+    authDomain: "js-fbauthentication.firebaseapp.com",
+    databaseURL: "https://js-fbauthentication.firebaseio.com",
+    projectId: "js-fbauthentication",
+    storageBucket: "js-fbauthentication.appspot.com",
+    messagingSenderId: "754142624518",
+    appId: "1:754142624518:web:8059b4055b3c39cd2f7c87",
+    measurementId: "G-NZ7RKVPDXB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+ let fbLogin = document.getElementById("facebooklogin")
+
+
+  fbLogin.addEventListener('click', signInWithFB);
+
+
+  let signInWithFB = () => {
+      
+  }
