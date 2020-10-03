506effa6dc18c7e5dcfa26287f9da0b58463f2fd
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-10-03 16:21:21 +0500

    added online users section

--- userInfo.js
+++ userInfo.js
@@ -1,6 +1,7 @@
 var userInfo = JSON.parse(localStorage.getItem("userInfo"));
 var crntUser = document.getElementById("currentUser");
 var userPhoto = document.getElementById("user_photo");
+var onlineUsers = document.getElementById("online_users");
 
 crntUser.innerHTML = localStorage.getItem("displayName");
 userPhoto.src = userInfo.photoURL;
